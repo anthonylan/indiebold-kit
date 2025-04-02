84cdc7b5eb8c00a87334ab93e65f444657ad264c
Anthony Charles <anthonylannn@gmail.com>
2025-04-02 14:18:04 +0200

    auth wrap adapted for ipads

--- components/Wrapper.vue
+++ components/Wrapper.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="grid grid-cols-1 md:grid-cols-2 min-h-screen bg-white dark:bg-[var(--dark-soft)]">
-       <div class="h-full flex flex-col md:flex-row items-center relative">
-           <div class="flex items-start relative md:absolute left-0 top-0 w-full px-5 md:px-10 pt-5">
+    <div class="grid grid-cols-1 lg:grid-cols-2 min-h-screen bg-white dark:bg-[var(--dark-soft)]">
+       <div class="h-full flex flex-col lg:flex-row items-center relative">
+           <div class="flex items-start relative lg:absolute left-0 top-0 w-full px-5 lg:px-10 pt-5">
              <div class="flex flex-1">
               <NuxtLink to="/login">
                 <Logo />
@@ -10,7 +10,7 @@
             <slot name="link"></slot>
           </div>
       
-          <div :class="[styleSheet.form.wrapper, 'mt-10 md:mt-auto']">
+          <div :class="[styleSheet.form.wrapper, 'mt-10 lg:mt-auto']">
             <div class="text-center mb-5">
               <h2 class="text-4xl font-bold mt-3 mb-2 font-title" :class="[styleSheet.title]">{{ title }}</h2>
               <p class="text-gray-600 dark:text-[var(--gray)]">{{ subtitle }}</p>
@@ -21,7 +21,7 @@
        </div>
 
        <!-- right -->
-        <div class="hidden md:flex items-end h-full relative" id="cover">
+        <div class="hidden lg:flex items-end h-full relative" id="cover">
           <div class="flex absolute w-full h-full bg-black/30 top-0 left-0 z-1"></div>
 
           <div class="flex flex-col justify-center gap-3 max-w-md mx-auto py-15 text-center relative z-5">
